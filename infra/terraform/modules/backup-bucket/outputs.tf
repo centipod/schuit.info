a08dddafe778b0e900b83f20e0343f5aa5db81ec
Christian Schuit <christian.schuit@centipod.nl>
2026-07-29 14:56:49 +0200

    Harden production web surface

--- infra/terraform/modules/backup-bucket/outputs.tf
+++ infra/terraform/modules/backup-bucket/outputs.tf
@@ -9,8 +9,3 @@ output "bucket_arn" {
 output "access_key_id" {
   value = aws_iam_access_key.this.id
 }
-
-output "secret_access_key" {
-  value     = aws_iam_access_key.this.secret
-  sensitive = true
-}
--- wordpress-mu-plugins/disable-canonical-redirect.php
+++ wordpress-mu-plugins/disable-canonical-redirect.php
@@ -5,3 +5,13 @@ declare(strict_types=1);
 add_filter('redirect_canonical', static function ($redirect_url) {
     return false;
 });
+
+add_filter('rest_endpoints', static function (array $endpoints): array {
+    if (is_user_logged_in()) {
+        return $endpoints;
+    }
+
+    unset($endpoints['/wp/v2/users'], $endpoints['/wp/v2/users/(?P<id>[\\d]+)']);
+
+    return $endpoints;
+});
